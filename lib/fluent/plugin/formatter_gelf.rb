0afaed5fd85767bd913f7fcb77906f18117016a1
Alex Yamauchi <oss@hotschedules.com>
2018-04-09 19:11:49 +0000

    INFRA-6817: add exception handlers so that buffers don't get permanently stuck trying to flush

--- lib/fluent/plugin/formatter_gelf.rb
+++ lib/fluent/plugin/formatter_gelf.rb
@@ -4,6 +4,10 @@ module Fluent
   module TextFormatter
     class GELFFormatter < Formatter
 
+      unless method_defined?(:log)
+        define_method('log') { $log }
+      end
+
       Plugin.register_formatter("gelf", self)
 
       require 'fluent/gelf_util'
@@ -17,15 +21,26 @@ module Fluent
       end
 
       def format(tag, time, record)
-        gelfentry = make_gelfentry(
-          tag,time,record,
-          {
-            :use_record_host => @use_record_host,
-            :add_msec_time => @add_msec_time
-          }
-        )
 
-        make_json(gelfentry,{})
+        begin
+
+          gelfentry = make_gelfentry(
+            tag,time,record,
+            {
+              :use_record_host => @use_record_host,
+              :add_msec_time => @add_msec_time
+            }
+          )
+
+          make_json(gelfentry,{})
+      
+        rescue Exception => e
+          log.error sprintf(
+            'Error trying to serialize %s: %s',
+            record.to_s.force_encoding('UTF-8'),
+            e.message.to_s.force_encoding('UTF-8')
+          )
+        end
       end
 
     end
